a71a5570bcfe0cc4ac3affce1aacb8766e02d735
zhangguichuan <acerDebugman@qq.com>
2024-01-24 23:36:10 +0800

    add 84

---
+++ leetcode/src/bin/largest-rectangle-in-histogram.rs
@@ -0,0 +1,19 @@
+/*
+给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+求在该柱状图中，能够勾勒出来的矩形的最大面积。
+
+输入：heights = [2,1,5,6,2,3]
+输出：10
+解释：最大的矩形为图中红色区域，面积为 10
+*/
+
+struct Solution;
+impl Solution {
+    pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
+
+    }
+}
+
+pub fn main() {
+
+}
