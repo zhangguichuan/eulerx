e31879297bdf56239b9b30d1bf52d5869b70f597
zhangguichuan <zhangguichuan@starcross.tech>
2024-03-01 18:35:12 +0800

    add 240

---
+++ leetcode/src/bin/240_search-a-2d-matrix-ii.rs
@@ -0,0 +1,22 @@
+/*
+编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+
+每行的元素从左到右升序排列。
+每列的元素从上到下升序排列。
+
+*/
+
+struct Solution;
+impl Solution {
+    pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
+        
+    }
+}
+
+pub fn main() {
+    let nums = vec![1,2,3,4];    
+    println!("{:?}", nums);
+    let ans = Solution::search_matrix(nums);
+    println!("ans: {:?}", ans);
+}
+
