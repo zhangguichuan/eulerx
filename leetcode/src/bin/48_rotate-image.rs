04c61cecd79bd9ada7987216cbc0dd8e4783a26a
zhangguichuan <zhangguichuan@starcross.tech>
2024-03-01 15:14:26 +0800

    fix 48

--- leetcode/src/bin/48_rotate-image.rs
+++ leetcode/src/bin/48_rotate-image.rs
@@ -10,14 +10,53 @@
 
 struct Solution;
 impl Solution {
-    pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
-        let (m, n) = (matrix.len(), matrix[0].len());
-         
+    pub fn swap(v1: &mut i32, v2: &mut i32) {
+        let tmp = *v1;
+        *v1 = *v2;
+        *v2 = tmp;
+    }
+
+    pub fn rotate(mat: &mut Vec<Vec<i32>>) {
+        let n = mat.len();
+        let mut l = (n - 1) as i32;
+        let (mut r, mut c) = (0, 0);
+        while l > 0 {
+            r = (n - l as usize)>>1;
+            
+            for k in 0..l as usize {
+                c = r+k;
+                let mut tmp = mat[r][c];
+                let mut rr = c;
+                let mut cc = n - 1 - r;
+                //println!("{:?}", mat);
+                for _ in 0..4 {
+                    //println!("{r},{c},{rr},{cc}"); 
+                    Self::swap(&mut mat[rr][cc], &mut tmp);
+
+                    r = rr;
+                    c = cc;
+                    rr = c;
+                    cc = n - 1 - r;
+                }
+            }
+
+            l -= 2 ;
+        }
     }
 }
 
 pub fn main() {
     let mut nums = vec![vec![1,2,3],vec![4,5,6],vec![7,8,9]];
+    println!("{:?}", nums);
+    Solution::rotate(&mut nums);
+    println!("ans: {:?}", nums);
+
+    let mut nums = vec![vec![1,2],vec![3,4]];
+    println!("{:?}", nums);
+    Solution::rotate(&mut nums);
+    println!("ans: {:?}", nums);
+
+    let mut nums = vec![vec![1]];
     println!("{:?}", nums);
     Solution::rotate(&mut nums);
     println!("ans: {:?}", nums);
